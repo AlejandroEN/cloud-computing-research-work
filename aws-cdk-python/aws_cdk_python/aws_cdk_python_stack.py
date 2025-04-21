f0b2146c7c2cf5ea257c319da49e6cbe7292b6f5
AlejandroEN <alejandroescobarnunez@gmail.com>
2025-04-20 22:20:08 -0500

    feat: refactor stack implementation to use EC2InstanceStack and remove unused test files

--- aws-cdk-python/aws_cdk_python/aws_cdk_python_stack.py
+++ aws-cdk-python/aws_cdk_python/aws_cdk_python_stack.py
@@ -1,16 +1,55 @@
-from aws_cdk import Stack  # Duration,; aws_sqs as sqs,
+import os.path
+
+from aws_cdk import Stack
+from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_iam as iam
 from constructs import Construct
 
+dirname = os.path.dirname(__file__)
 
-class AwsCdkPythonStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+class EC2InstanceStack(Stack):
 
-        # The code that defines your stack goes here
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdkPythonQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        vpc = ec2.Vpc(
+            self,
+            "VPC",
+            nat_gateways=0,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="public", subnet_type=ec2.SubnetType.PUBLIC
+                )
+            ],
+        )
+
+        cloud9_image = ec2.MachineImage.generic_linux(
+            {self.region: "ami-022ce79dc9cabea0c"}
+        )
+
+        security_group = ec2.SecurityGroup(
+            self,
+            "InstanceSG",
+            vpc=vpc,
+            description="Allow SSH and HTTP",
+            allow_all_outbound=True,
+        )
+        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), "SSH")
+        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "HTTP")
+
+        instance = ec2.Instance(
+            self,
+            "Instance",
+            instance_type=ec2.InstanceType("t3.nano"),
+            machine_image=cloud9_image,
+            vpc=vpc,
+            key_name="vockey",
+            security_group=security_group,
+            block_devices=[
+                ec2.BlockDevice(
+                    device_name="/dev/xvda",
+                    volume=ec2.BlockDeviceVolume.ebs(20),
+                )
+            ],
+        )
